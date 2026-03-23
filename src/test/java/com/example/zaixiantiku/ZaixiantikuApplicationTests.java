844866a57332943f1b24a5180ae4053188a43abd
yangzhiyun <yangzhiyunppp@outlook.com>
2026-03-24 00:27:57 +0800

    初始代码

---
+++ src/test/java/com/example/zaixiantiku/ZaixiantikuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zaixiantiku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaixiantikuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
